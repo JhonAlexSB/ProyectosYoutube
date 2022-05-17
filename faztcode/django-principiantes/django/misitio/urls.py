a6b712c24c3b478bcf38f4ab74c31073c81d3978
JhonAlexTVBox <jhonalexsalazar04@gmail.com>
2022-05-17 03:51:15 -0500

    Modificado notasDjango seccion "Hello World" y archivos views.py urls.py

--- faztcode/django-principiantes/django/misitio/urls.py
+++ faztcode/django-principiantes/django/misitio/urls.py
@@ -15,7 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+# from miapp.views import hello
+from miapp import views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # path('', hello)
+    path('', views.hello),
+    path('about/', views.about)
 ]
+
