8585b44b80f76136db69027017d9aac48839fac8
JhonAlexSB <jhonalexsalazar04@gmail.com>
2022-10-27 10:02:20 -0500

    Modificados urls.py views.py notasDjango de seccion "Paramas" agregado insercion por url hello/

--- faztcode/django-principiantes/django/miapp/views.py
+++ faztcode/django-principiantes/django/miapp/views.py
@@ -2,7 +2,8 @@
 from django.http import HttpResponse
 
 # Create your views here.
-def hello(request):
+def hello(request, username):
+    print(username)
     return HttpResponse("<h2> Hello World</h2>")
 
 def about(request):
